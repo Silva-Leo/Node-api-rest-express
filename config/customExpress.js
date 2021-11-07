4d7a8d06a6ab99c04f8d70e9a9e955552d76d94a
Leo Silva <leonardo.ghost@gmail.com>
2021-11-06 21:05:22 -0300

    100% curso

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,10 +1,18 @@
-const express = require('express') //*importou a biblioteca express
+const express = require('express') //*importou o framework express
 
 const consign = require('consign') //* importou o consign
+
+//const bodyParser = require('body-parser') //* importou o body-parser
 
 module.exports = () => {
 
     const app = express() //* criou um app com as funções executadas do express
+
+    app.use(express.urlencoded())
+    //app.use(bodyParser.urlencoded({extended: true})) //* Aqui estamos dizendo para o express usar o bodyparses para converter os dados da requisição
+    //**O body-Parser caiu em desuso, o próprio Express implementou esse middlewares */
+    // app.use(body-parser.json())
+    app.use(express.json())
 
     consign()
         .include('controllers')
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 
 const customExpress = require('./config/customExpress')
 
-const app = customExpress()
+const conexao = require('./infraestrutura/conexao')
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000')) //*subiu o servidor
+const Tabelas = require('./infraestrutura/tabelas')
+
+conexao.connect((erro) => {
+    if (erro) {
+        console.log(erro);
+    } else {
+        console.log("conectado com sucesso");
+
+        Tabelas.init(conexao);
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('servidor rodando na porta 3000')) //*subiu o servidor
+
+    }
+})
 
